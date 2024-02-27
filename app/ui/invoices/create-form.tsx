c97a6b926cde54542a947b7cff34db44835c9d28
hickatheworld <cc@hicka.world>
2024-02-27 22:56:08 +0100

    Chapter 14

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -8,11 +9,14 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
+import {useFormState} from 'react-dom';
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+	const initialState = { message: null, errors: {} };
+	const [state, dispatch] = useFormState(createInvoice, initialState);
 	return (
-		<form action={createInvoice}>
+		<form action={dispatch}>
 			<div className="rounded-md bg-gray-50 p-4 md:p-6">
 				{/* Customer Name */}
 				<div className="mb-4">
@@ -23,8 +27,10 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 						<select
 							id="customer"
 							name="customerId"
+							required
 							className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
 							defaultValue=""
+							aria-describedby="customer-error"
 						>
 							<option value="" disabled>
 								Select a customer
@@ -35,7 +41,16 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 								</option>
 							))}
 						</select>
-						<UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+						<UserCircleIcon
+							className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"/>
+					</div>
+					<div id="customer-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.customerId &&
+							state.errors.customerId.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -50,6 +65,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 								id="amount"
 								name="amount"
 								type="number"
+								required
 								step="0.01"
 								placeholder="Enter USD amount"
 								className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -72,6 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 									name="status"
 									type="radio"
 									value="pending"
+									required
 									className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
 								/>
 								<label
